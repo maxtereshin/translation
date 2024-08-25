b2b6cf6119f890452f9f86af987c1c8911e568c6
James Ha <ngodanghuuha@gmail.com>
2024-08-25 15:21:32 +0700

    Update 2013_07_25_145943_create_languages_table.php

--- database/migrations/2013_07_25_145943_create_languages_table.php
+++ database/migrations/2013_07_25_145943_create_languages_table.php
@@ -14,8 +14,11 @@ class CreateLanguagesTable extends Migration
     {
         Schema::connection(config('translator.connection'))->create('translator_languages', function ($table) {
             $table->increments('id');
-            $table->string('locale', 6)->unique();
-            $table->string('name', 60)->unique();
+            $table->string('locale', 20)->unique()->nullable(false);
+            $table->string('name', 60)->unique()->nullable(false);
+            $table->integer('sort')->default(0)->nullable();
+            $table->string('logo')->nullable();
+            $table->enum('status', ['active', 'inactive'])->default('active');
             $table->timestamps();
             $table->softDeletes();
         });
